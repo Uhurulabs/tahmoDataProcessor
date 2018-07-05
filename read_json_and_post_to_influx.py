cc5b49a2b6bf67ab7e2a26582245e9b6342d353d
evilGmonkey <freddie@uhurunet.com>
2018-07-05 09:42:33 +0300

    Works to post JSON data from TAHMO API to Influxdb

--- read_json_and_post_to_influx.py
+++ read_json_and_post_to_influx.py
@@ -24,8 +24,9 @@ def getDirectoryFileList(dataPath):
 
 json_files = getDirectoryFileList(dataPath)
 
-for json_file in json_files:
+for json_file in sorted(json_files):
     json_file = dataPath + json_file
+    print "FILE : ", json_file
     with open(json_file) as f:
         data = json.load(f)
 
@@ -45,55 +46,69 @@ for json_file in json_files:
         variableName[count] = variable
     #print data['station']['variables']
     #print stationName
-    print json_file
     data_points = []
     payload_line = ""
     logLine = ""
-    for time in sorted(data['timeseries'][variableName[1]].keys()):
-            payload_line = "reading,station_id=" + station_id + ",station_name=" + stationName
-            payload_line = payload_line + ",station_elevation=" + stationElevation + \
-                ",station_long=" + stationLong + ",station_lat=" + stationLat
-            payload_line = payload_line + " "
-#            print time, payload_line
-#            print data['timeseries'].keys()
-#            for key in sorted(data['timeseries'].keys()):
-#                print key
-#            print "test line: ", test_line
-            count = 0
-            missing_variables = ""
-            while count < amountVariables:
-                count = count + 1
-                if count == 1:
-                    payload_line = payload_line + \
-                        str(variableName[count]) + "=" + \
-                        str(data['timeseries'][variableName[count]][time])
-                else:
-                    if str(variableName[count]) in data['timeseries'].keys():
-                        try:
-                            payload_line = payload_line + "," + \
+    totalValues = 0
+    count = 0
+    # Count how many values are in the json file
+    for key in data['timeseries'].keys():
+        count = count +1
+        try:
+            totalValues = totalValues + len((data['timeseries'][variableName[count]].keys()))
+        except:
+            pass
+#    print json_file, " LENGTH : ", totalValues
+    if totalValues > 0:
+        try:
+            for time in sorted(data['timeseries'][variableName[1]].keys()):
+                    payload_line = "reading,station_id=" + station_id + ",station_name=" + stationName
+                    payload_line = payload_line + ",station_elevation=" + stationElevation + \
+                        ",station_long=" + stationLong + ",station_lat=" + stationLat
+                    payload_line = payload_line + " "
+                    count = 0
+                    missing_variables = ""
+                    while count < amountVariables:
+                        count = count + 1
+                        if count == 1:
+                            payload_line = payload_line + \
                                 str(variableName[count]) + "=" + \
                                 str(data['timeseries'][variableName[count]][time])
-                        except:
-                            missing_variables = missing_variables + str(variableName[count]) + " "
+                        else:
+                            if str(variableName[count]) in data['timeseries'].keys():
+                                try:
+                                    payload_line = payload_line + "," + \
+                                        str(variableName[count]) + "=" + \
+                                        str(data['timeseries'][variableName[count]][time])
+                                except:
+                                    missing_variables = missing_variables + str(variableName[count]) + " "
 
 
-            payload_line = payload_line + " " + convertToUnixTimeStamp(time)
-            logLine =  "READINGTIME : "+ time + " : FROM : " + json_file + " : POSTING : " + payload_line
-            logLine = logLine + " : MISSINGVARS : " + missing_variables + "\n"
+                    payload_line = payload_line + " " + convertToUnixTimeStamp(time)
+                    logLine =  "READINGTIME : "+ time + " : FROM : " + json_file + " : POSTING : " + payload_line
+                    logLine = logLine + " : MISSINGVARS : " + missing_variables + "\n"
+                    data_points.append(payload_line)
+                    payload = "\n".join(data_points)
+                    #print payload
+
+            count = 0
+            for payload in data_points:
+                count = count + 1
+                #print count, payload
+                url = "http://" + influxDbHost + ":8086/write"
+                qs = {"db": "env_readings", 'precision': 's'}
+                response = requests.request("POST", url, data=payload, params=qs)
+                #                            auth=('admin', '2015IS4us!'))
+                #print payload
+                #print url, data, qs
+                #print response.text
             print logLine
             log.write(logLine)
-            data_points.append(payload_line)
-            payload = "\n".join(data_points)
-            #print payload
-
-    count = 0
-    for payload in data_points:
-        count = count + 1
-        #print count, payload
-        url = "http://" + influxDbHost + ":8086/write"
-        qs = {"db": "env_readings", 'precision': 's'}
-        response = requests.request("POST", url, data=payload, params=qs)
-#                                    auth=('admin', '2015IS4us!'))
-        #print payload
-#        print url, data, qs
-#        print response.text
+        except:
+            pass
+    else:
+        try:
+            logLine = "READINGTIME : "+ time + " : FROM : " + json_file + " : NOREADINGS" + "\n"
+        except:
+            logLine = "READINGTIME : "+ "FIRSTFILEINLIST" + " : FROM : " + json_file + " : NOREADINGS" + "\n"
+        log.write(logLine)
